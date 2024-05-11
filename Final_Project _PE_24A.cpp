25f40e94c3b059f85e6944ba8204c26d666303b0
gesqued4 <gerardo.esqueda9479@alumnos.udg.mx>
2024-05-11 16:21:30 -0600

    Some changes on the mian file

--- Final_Project _PE_24A.cpp
+++ Final_Project _PE_24A.cpp
@@ -1,1 +1,34 @@
-//archivo main
+/*************************Presentacion*************************
+        PROGRAMA: Final_Project_PE_24A.cpp
+        CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+        INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+        PROFESOR: Sergio Franco Casillas
+        DESCRIPCION: Programa para universidad de prestigio
+**************************************************************/
+#include <iostream>
+using namespace std;
+
+bool ciclo = true;
+char op_switch;
+
+int main(){
+
+while(ciclo){
+    cout << "UndaPRO by UdeG\t\t\t\t\t\t\tUniversity Professional Data Management" << endl;
+    cout << "\t1. Agregar estudiantes" << endl;
+    cin >> op_switch;
+    system("cls");
+    switch(op_switch){
+        case '1': // Case 1: Agregar estudiantes
+            cout << "UndaPRO by UdeG\t\t\t\t\t\t\tUniversity Professional Data Management" << endl;
+            cout << "//Agregar estudiantes" << endl;
+            cout << "\nCompleta los siguientes campos para registrar los datos del estudiante en nuestro sistema." << endl;
+            cin >> op_switch;
+            system("cls");
+        break;  // Fin case 1
+        default:
+            cout << "Opcion invalida, seleccione una opcion correcta..." << endl;
+        }   // Fin switch principal del programa
+    }   // Fin ciclo general del programa
+return 0;
+}   // Fin main
